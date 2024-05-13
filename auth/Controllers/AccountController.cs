c20e641a8296ed8867bc7cbdfc8b5a77e43c698b
silenceafter <a59-info@yandex.ru>
2024-05-13 15:49:46 +0300

    добавлены контроллеры и сервисы

---
+++ auth/Controllers/AccountController.cs
@@ -0,0 +1,31 @@
+﻿//using auth.Areas.Identity.Pages.Account;
+using auth.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace auth.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AccountController : Controller
+    {
+        private readonly AccountService _accountService;
+
+        public AccountController(AccountService accountService)
+        {
+            _accountService = accountService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            return Ok();
+        }
+
+        [HttpGet("register/{jj?}")]
+        public async Task<IActionResult> Register([FromQuery] string email, string password)
+        {
+            await _accountService.Register(email, password);
+            return Ok();
+        }
+    }
+}
---
+++ auth/Services/AccountService.cs
@@ -0,0 +1,33 @@
+﻿//using auth.Areas.Identity.Pages.Account;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Net;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace auth.Services
+{
+    public class AccountService
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public AccountService(UserManager<IdentityUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<string> Register(/*RegisterModel model*/string email, string password)
+        {
+            var user = new IdentityUser { UserName = email, Email = email };
+            var result = await _userManager.CreateAsync(user, password);
+            return "1";
+        }
+    }
+}
